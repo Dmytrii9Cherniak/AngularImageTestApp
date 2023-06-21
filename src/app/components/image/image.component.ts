ae6bf51a0994fe4a27d538d2c9a5d027a2ebf5ef
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-06-21 21:26:17 +0300

    new code

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,31 +1,123 @@
-import {AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+// import {
+//   AfterViewInit,
+//   Component,
+//   ElementRef,
+//   HostListener,
+//   ViewChild
+// } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-image',
+//   templateUrl: './image.component.html',
+//   styleUrls: ['./image.component.scss']
+// })
+// export class ImageComponent implements AfterViewInit {
+//
+//   @ViewChild('imageRef', { static: true }) imageElement: ElementRef;
+//
+//   public canvas: HTMLCanvasElement;
+//   public context: CanvasRenderingContext2D;
+//   public startX: number = 0;
+//   public startY: number = 0;
+//   public endX: number = 0;
+//   public endY: number = 0;
+//
+//   public ngAfterViewInit() {
+//     this.canvas = this.imageElement.nativeElement;
+//     this.context = this.canvas.getContext('2d')!;
+//
+//     if (this.context) {
+//       const image: HTMLImageElement = new Image();
+//       image.src = 'assets/media/img.png';
+//       image.onload = () => {
+//         this.context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+//       };
+//     }
+//   }
+//
+//   public drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+//     console.log(context)
+//     context.beginPath();
+//     context.moveTo(x1, y1);  // Встановлюємо початкову точку
+//     context.lineTo(x2, y2);  // Встановлюємо кінцеву точку
+//     context.lineWidth = 1
+//     context.stroke();  // Малюємо лінію
+//   }
+//
+//   @HostListener('touchstart', ['$event'])
+//   public canvasOnMouseDown(event: MouseEvent) {
+//     const rect: DOMRect = this.canvas.getBoundingClientRect();
+//     this.startX =  event.clientX - rect.x;
+//     this.startY = event.clientY - rect.y;
+//     console.log(this.startY, this.startY, 'x start', 'y start')
+//   }
+//
+//   @HostListener('mouseup', ['$event'])
+//   public canvasOnMouseUp(event: MouseEvent) {
+//     const rect: DOMRect = this.canvas.getBoundingClientRect();
+//     this.endX = event.clientX  - rect.x;
+//     this.endY = event.clientY - rect.y;
+//     this.drawLine(this.context, this.startX, this.startY, this.endX, this.endY);
+//     console.log(this.endX, this.endY, 'x end', 'y end')
+//   }
+//
+// }
+
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent implements OnInit, AfterViewInit {
+export class ImageComponent implements AfterViewInit {
 
   @ViewChild('imageRef', { static: true }) imageElement: ElementRef;
 
-
-  ngOnInit() {
-
-  }
+  public canvas: HTMLCanvasElement;
+  public context: CanvasRenderingContext2D;
+  public startX: number = 0;
+  public startY: number = 0;
+  public endX: number = 0;
+  public endY: number = 0;
 
   public ngAfterViewInit() {
-    const canvasElement: HTMLCanvasElement = this.imageElement.nativeElement;
-    const image = new Image();
-    canvasElement.width = 1400;
-    canvasElement.height = 650;
-    image.src = 'assets/media/img.png';
-    image.onload = () => {
-      const context = canvasElement.getContext('2d');
-      if (context) {
-        context.drawImage(image, 0, 0, canvasElement.width, canvasElement.height);
-      }
-    };
+    this.canvas = this.imageElement.nativeElement;
+    this.context = this.canvas.getContext('2d')!;
+
+    if (this.context) {
+      const image: HTMLImageElement = new Image();
+      image.src = 'assets/media/img.png';
+      image.onload = () => {
+        this.context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+      };
+    }
+  }
+
+  public drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    console.log(context)
+    context.beginPath();
+    context.moveTo(x1, y1);  // Встановлюємо початкову точку
+    context.lineTo(x2, y2);  // Встановлюємо кінцеву точку
+    // context.lineWidth = 1
+    context.stroke();  // Малюємо лінію
+  }
+
+  @HostListener('mousedown', ['$event'])
+  public canvasOnMouseDown(event: MouseEvent) {
+    const rect: DOMRect = this.canvas.getBoundingClientRect();
+    this.startX =  event.clientX - rect.x;
+    this.startY = event.clientY - rect.y;
+    console.log(this.startX, this.startY, 'x start', 'y start')
+  }
+
+  @HostListener('mouseup', ['$event'])
+  public canvasOnMouseUp(event: MouseEvent) {
+    const rect: DOMRect = this.canvas.getBoundingClientRect();
+    this.endX = event.clientX  - rect.x;
+    this.endY = event.clientY - rect.y;
+    this.drawLine(this.context, this.startX, this.startY, this.endX, this.endY);
+    console.log(this.endX, this.endY, 'x end', 'y end')
   }
 
 }
