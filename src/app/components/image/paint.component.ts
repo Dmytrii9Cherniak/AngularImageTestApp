32575f2ee77ed987a66f548528203f38a2236d60
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-06-27 21:28:40 +0300

    added point center line

--- src/app/components/image/paint.component.ts
+++ src/app/components/image/paint.component.ts
@@ -68,46 +68,53 @@ export class PaintComponent implements AfterViewInit {
     }
   }
 
-  public draw(event?: MouseEvent) :void {
+  public draw(event?: MouseEvent): void {
     if (!this.context || !this.backgroundImage) return;
     const canvas = this.canvas?.nativeElement;
     if (canvas) {
       this.context.clearRect(0, 0, canvas.width, canvas.height);
       this.context.drawImage(this.backgroundImage, 0, 0, canvas.width, canvas.height);
-      this.drawLines();
+
+      for (const line of this.lines) {
+        this.context.beginPath();
+        this.context.moveTo(line.start.x, line.start.y);
+        this.context.lineTo(line.end.x, line.end.y);
+        this.context.stroke();
+        this.drawMidPoint(line.start, line.end);
+      }
+
       if (this.isDrawing && event && this.startPoint) {
+        const currentPoint = { x: event.offsetX, y: event.offsetY };
         this.context.beginPath();
         this.context.moveTo(this.startPoint.x, this.startPoint.y);
-        this.context.lineTo(event.offsetX, event.offsetY);
+        this.context.lineTo(currentPoint.x, currentPoint.y);
         this.context.stroke();
-        this.midPoint = this.calculateMidPoint(this.startPoint, { x: event.offsetX, y: event.offsetY });
-        this.drawMidPoint();
-      } else if (this.midPoint) {
-        this.drawMidPoint();
+        this.drawMidPoint(this.startPoint, currentPoint);
       }
     }
   }
 
-  public stopDrawing(event: MouseEvent) :void {
+  public stopDrawing(event: MouseEvent): void {
     if (this.isDrawing && event && this.startPoint) {
       const canvas = this.canvas?.nativeElement;
       if (canvas) {
         const endPoint = { x: event.offsetX, y: event.offsetY };
         this.lines.push({ start: this.startPoint, end: endPoint });
         this.startPoint = null;
-        this.midPoint = null;
         this.isDrawing = false;
         this.draw();
       }
     }
   }
 
-  public drawMidPoint() :void {
-    if (!this.context || !this.midPoint) return;
-    this.context.beginPath();
-    this.context.arc(this.midPoint.x, this.midPoint.y, 5, 0, 2 * Math.PI);
-    this.context.fillStyle = 'red';
-    this.context.fill();
+  public drawMidPoint(startPoint: { x: number; y: number }, endPoint: { x: number; y: number }): void {
+    const midPoint = this.calculateMidPoint(startPoint, endPoint);
+    if (this.context) {
+      this.context.beginPath();
+      this.context.arc(midPoint.x, midPoint.y, 5, 0, 2 * Math.PI);
+      this.context.fillStyle = 'red';
+      this.context.fill();
+    }
   }
 
   public handleClick(event: MouseEvent) {
@@ -135,15 +142,4 @@ export class PaintComponent implements AfterViewInit {
     return dx * dx + dy * dy <= radius * radius;
   }
 
-  public drawLines() :void {
-    if (!this.context) return;
-    this.context.strokeStyle = 'black';
-    this.context.lineWidth = 2;
-    for (const line of this.lines) {
-      this.context.beginPath();
-      this.context.moveTo(line.start.x, line.start.y);
-      this.context.lineTo(line.end.x, line.end.y);
-      this.context.stroke();
-    }
-  }
 }
